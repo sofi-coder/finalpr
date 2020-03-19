4da13504b78228a1bf9736a4e9e001f386349138
sofi-coder <sofi_kovaluk@ukr.net>
2020-03-19 20:39:07 +0000

    Sample code

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const workButton = document.getElementById("work_button");
 const servicesButton = document.getElementById("services_button");
 const contactsButton = document.getElementById("contacts_button");
 const aboutSalon = document.getElementById("about_salon");
+const ourWork = document.getElementById("our_work");
 
 
 
@@ -17,7 +18,8 @@ var uaTexts = {
     work: "Наші роботи ",
     services: "Послуги",
     contacts: "Контакти",
-    aboutSalon: "Салон краси Evi у Львові пропонує повний спектр процедур догляду та працює виключно з професійною косметикою. Бути красивою та доглянутою - це мрія кожної жінки. Наші спеціалісти докладуть усіх зусиль, щоб здивувати вас. Ми також єдиний салон на вулиці Володимира Великого, де персонал розмовляє англійською. Ми знаємо секрет, як підкреслити вашу природну привабливість і уповільнити плин часу. У нашому салоні краси завжди по-домашньому затишна і доброзичлива атмосфера, увага і турбота про кожного клієнта. Це те місце, куди вам захочеться повернутися знову і знову. "
+    aboutSalon: "Салон краси Evi у Львові пропонує повний спектр процедур догляду та працює виключно з професійною косметикою. Бути красивою та доглянутою - це мрія кожної жінки. Наші спеціалісти докладуть усіх зусиль, щоб здивувати вас. Ми також єдиний салон на вулиці Володимира Великого, де персонал розмовляє англійською. Ми знаємо секрет, як підкреслити вашу природну привабливість і уповільнити плин часу. У нашому салоні краси завжди по-домашньому затишна і доброзичлива атмосфера, увага і турбота про кожного клієнта. Це те місце, куди вам захочеться повернутися знову і знову. ",
+    ourWork: "Наші роботи"
     
 };
 
@@ -26,7 +28,8 @@ var enTexts = {
     work: "Our Work",
     services: "Services",
     contacts: "Contacts",
-    aboutSalon: "weräiljeriojgoäitej"
+    aboutSalon: "Beauty Salon Evi in Lviv offers a full range of care procedures and works exclusively with professional cosmetics. To be beauttul and well- groomed is every woman's dream Our specialsts will make every effort to surprise you with the result. We are also the only salon on Volodymyra Velykogo street where staff speak English. Also we have friendly atmpthphere and pesonal approrach to each client.",
+    ourWork: "Our Work"
 };
 
 
@@ -42,6 +45,7 @@ function switchLanguage() {
       servicesButton.innerHTML = enTexts.services;
       contactsButton.innerHTML = enTexts.contacts;
       aboutSalon.innerHTML = enTexts.aboutSalon;
+      ourWork.innerHTML = enTexts.ourWork;
           language = "en";
 
 
@@ -53,6 +57,7 @@ function switchLanguage() {
       servicesButton.innerHTML = uaTexts.services;
       contactsButton.innerHTML = uaTexts.contacts;
       aboutSalon.innerHTML = uaTexts.aboutSalon;
+      ourWork.innerHTML = uaTexts.ourWork;
 
      language = "ua";
 
