6f8deaedd3e4fc0811b1f7713f4eb3ca63767b75
sofi-coder <sofi_kovaluk@ukr.net>
2020-03-20 16:53:58 +0000

    Sample code

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const servicesButton = document.getElementById("services_button");
 const contactsButton = document.getElementById("contacts_button");
 const aboutSalon = document.getElementById("about_salon");
 const ourWork = document.getElementById("our_work");
+const ourServices = document.getElementById("our_services");
 
 
 
@@ -19,7 +20,8 @@ var uaTexts = {
     services: "Послуги",
     contacts: "Контакти",
     aboutSalon: "Салон краси Evi у Львові пропонує повний спектр процедур догляду та працює виключно з професійною косметикою. Бути красивою та доглянутою - це мрія кожної жінки. Наші спеціалісти докладуть усіх зусиль, щоб здивувати вас. Ми також єдиний салон на вулиці Володимира Великого, де персонал розмовляє англійською. Ми знаємо секрет, як підкреслити вашу природну привабливість і уповільнити плин часу. У нашому салоні краси завжди по-домашньому затишна і доброзичлива атмосфера, увага і турбота про кожного клієнта. Це те місце, куди вам захочеться повернутися знову і знову. ",
-    ourWork: "Наші роботи"
+    ourWork: "Наші роботи",
+    ourServices: "Наші послуги"
     
 };
 
@@ -29,7 +31,8 @@ var enTexts = {
     services: "Services",
     contacts: "Contacts",
     aboutSalon: "Beauty Salon Evi in Lviv offers a full range of care procedures and works exclusively with professional cosmetics. To be beauttul and well- groomed is every woman's dream Our specialsts will make every effort to surprise you with the result. We are also the only salon on Volodymyra Velykogo street where staff speak English. Also we have friendly atmpthphere and pesonal approrach to each client.",
-    ourWork: "Our Work"
+    ourWork: "Our Work",
+    ourServices: "Our Services"
 };
 
 
@@ -46,6 +49,7 @@ function switchLanguage() {
       contactsButton.innerHTML = enTexts.contacts;
       aboutSalon.innerHTML = enTexts.aboutSalon;
       ourWork.innerHTML = enTexts.ourWork;
+      ourServices.innerHTML = enTexts.Services;
           language = "en";
 
 
@@ -58,7 +62,7 @@ function switchLanguage() {
       contactsButton.innerHTML = uaTexts.contacts;
       aboutSalon.innerHTML = uaTexts.aboutSalon;
       ourWork.innerHTML = uaTexts.ourWork;
-
+      ourServices.innerHTML = uaTexts.ourServices;
      language = "ua";
 
  }
